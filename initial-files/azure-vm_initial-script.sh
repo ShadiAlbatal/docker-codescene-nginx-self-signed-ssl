55dd135ffa2a61cccde3e59b1998b629e39ef6b7
shadialbatal <shadialbatal1993@hotmail.com>
2021-10-11 16:36:34 +0200

    updating the file, to be more automated

--- initial-files/azure-vm_initial-script.sh
+++ initial-files/azure-vm_initial-script.sh
@@ -1,37 +1,30 @@
-#!/bin/bash
+#!/bin/bash -i
 
-
-echo "CodeScene"
-cd /home/$user
-pwd
-dir=`pwd`
-$dir
+echo ""
+#cd /home/$user
 echo "update"
-apt update -y
+sudo apt update -y
 echo "----------------------------------------------------------"
 echo "upgrade"
-apt upgrade -y
+sudo apt upgrade -y
 echo "----------------------------------------------------------"
 echo "get install software-properties-common"
-apt-get install software-properties-common
+sudo apt-get install software-properties-common
 echo "----------------------------------------------------------"
 echo "repository universe"
-add-apt-repository universe
+sudo add-apt-repository universe
 echo "----------------------------------------------------------"
 echo "install docker"
-apt install docker.io -y && apt install docker-compose -y
+sudo apt install docker.io -y && sudo apt install docker-compose -y
 echo "----------------------------------------------------------"
 echo "sudo snap install core; sudo snap refresh core"
-snap install core; sudo snap refresh core
+sudo snap install core; sudo snap refresh core
 echo "----------------------------------------------------------"
 echo "snap install --classic certbot"
-snap install --classic certbot
+sudo snap install --classic certbot
 echo "----------------------------------------------------------"
 echo "ln -s /snap/bin/certbot /usr/bin/certbot"
-ln -s /snap/bin/certbot /usr/bin/certbot
-echo "----------------------------------------------------------"
-#dir=`pwd`
-#echo $dir
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
 echo "mkdir src , enter it"
 mkdir source
 cd source
@@ -41,15 +34,37 @@ mkdir repos
 cd repos
 echo "----------------------------------------------------------"
 echo "git clone"
-git clone https://github.com/ShadiAlbatal/docker-codescene-nginx-self-signed-ssl.git
+sudo git clone https://github.com/ShadiAlbatal/docker-codescene-nginx-self-signed-ssl.git
 echo "cd  docker-codescene-nginx-self-signed-ssl"
 cd docker-codescene-nginx-self-signed-ssl/
 echo "checkout  using_letsencrypt_ssl"
 git checkout using_letsencrypt_ssl
 echo "----------------------------------------------------------"
 echo "copy docker-compose to user dir"
-cp docker-compose-user/docker-compose.yml $dir
+cp initial-files/docker-compose.yml /home/$USER/docker-compose.yml /home/$USER
 echo "----------------------------------------------------------"
-cd $dir
 echo "create codescene"
-mkdir codescene
+mkdir -p /home/$USER/codescene
+
+echo "to issue a ssl-cert, provide the following data, and answer the question"
+echo "email"
+read email
+echo "dns"
+read dns
+
+echo $email
+echo $dns
+
+certbot certonly --standalone -d $dns --email $email
+
+
+filename="/home/azureuser/sda/df/gg/nginx.conf"
+
+read -p "Enter the replace string: " replace
+
+string="**domain_name**"
+
+if [[ $string != "" && $replace != "" ]]; then
+sed -i "s/$string/$dns/" $filename
+#echo "succ"
+fi
